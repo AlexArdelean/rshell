64ab1ce38c7347cdc8917a3d4dc45e2f4a5e52a7
Branden Moya <bmoya001@ucr.edu>
2016-01-31 21:04:28 -0800

    version 1.0 - trouble chaining cmds + other bugs

--- main.cpp
+++ main.cpp
@@ -1,27 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <cstdio>
 #include <string>
-#include <sstream>
+#include <cstdlib>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 //simple Base class
 class Base{
     public:
+    int status;
+
     Base() {};
 
     virtual bool execute() = 0;
 };
 
 //classes that inherit Base
-class Operand : public Base{
-    string data;
-
+class Commands : public Base{
     public:
-    string get_data(){
-	return data;
+    char *cmd[256];
+
+    Commands(char *line, int s){
+	status = s;
+	int i = 0;
+
+	char *chold = strtok(line, " ");
+	while(chold != NULL){
+	    cmd[i] = chold;
+	    chold = strtok(NULL, " ");
+	    i++;
+	}
+
+	cmd[i] = '\0';
+    }
+
+    bool execute(){
+	if(cmd[0] == "exit")
+	    exit(EXIT_SUCCESS);
+
+	pid_t pid = fork();
+	int ihold;
+
+	if(pid == -1){
+	    cout << "Forking Error!\n";
+	    exit(EXIT_FAILURE);
+	}
+
+	else if(pid == 0){
+	    if(execvp(*cmd, cmd) < 0){
+		cout << "Execution Error!\n";
+		exit(EXIT_FAILURE);
+	    }
+	}
+
+	else
+	   pid = wait(&ihold);
+
+	return true;
     }
 };
 
@@ -29,99 +70,86 @@ class Command_Line : public Base{
     public:
     vector<Base*> line;
     
-    Command_Line(string shold){
-	//create vector of Base* using shold
-    	
+    void get_vinfo(vector<int> &vstatus, vector<int> &vpos, char *chold){
+	for(int i = 0; i < strlen(chold); i++){
+	    if(chold[i] == ';'){
+		vstatus.push_back(0);
+		vpos.push_back(i+1);
+		chold[i] = '\0';
+	    }
 
+	    else if(chold[i] == '&' && chold[i+1] == '&'){
+		vstatus.push_back(1);
+		vpos.push_back(i+2);
+		chold[i] = '\0';
+	    }
+
+	    else if(chold[i] == '|' && chold[i+1] == '|'){
+		vstatus.push_back(2);
+		vpos.push_back(i+2);
+		chold[i] = '\0';
+	    }
+	}
+    }
+
+    Command_Line(char *input){
+	vector<int> vstatus;
+	vector<int> vpos;
+	get_vinfo(vstatus, vpos, input);
+
+	line.push_back(new Commands(input, 0));
+	for(int i = 0; i < vstatus.size(); i++)
+	    line.push_back(new Commands(input + vpos[i], vstatus[i]));
     }
     
-    int size(){
-	return line.size();
-    }
-};
-
-class Commands : public Base{
-    public:
-    int status;
-    vector<Base*> options;
-    virtual bool execute() = 0;
-};
-
-//classes the inherit Commands
-class LS_cmd : public Commands{
-    public:
     bool execute(){
-	cout << "LS_cmd: ok" << endl;
+	bool last_cmd_passed;
+
+	for(int i = 0; i < line.size(); i++){
+	    if(line[i]->status == 0)
+		last_cmd_passed= line[i]->execute();
+
+	    else if(line[i]->status == 1)
+		if(last_cmd_passed)
+		    last_cmd_passed= line[i]->execute();
+
+	    else if(line[i]->status == 2)
+		if(!last_cmd_passed)
+		    last_cmd_passed= line[i]->execute();
+	
+	    else
+		return false;
+	}
+
 	return true;
     }
 };
 
-class EXIT_cmd : public Commands{
-    public:
-    bool execute(){
-	cout << "EXIT_cmd: ok" << endl;
-	return true;
-    }
-};
-
-class MKDIR_cmd : public Commands{
-    public:
-    bool execute(){
-	cout << "MKDIR_cmd: ok" << endl;
-	return true;
-    }
-};
-
-class CD_cmd : public Commands{
-    public:
-    bool execute(){
-	cout << "CD_cmd: ok" << endl;
-	return true;
-    }
-};
-
-class ECHO_cmd : public Commands{
-    public:
-    bool execute(){
-	cout << "ECHO_cmd: ok" << endl;
-	return true;
-    }
-};
-
-void rm_comments(string &shold){
-    if(shold.find("#") != -1)
-	shold = shold.substr(0, shold.find());
+void rm_comments(char *input){
+    for(int i = 0; input[i] != '\0'; i++)
+	if(input[i] == '#')
+	    input[i] = '\0';
 }
 
 int main(){
-    string input;
-    bool last_cmd_passed;
+    bool run = true;
 
-    while(true){
+    while(run){
 	//output propt (include username/location for extra credit)
 	cout << "$ ";
 
 	//get arguement(s)
-    	getline(cin, input);
-
+    	char input[1024];
+	cin.getline(input, 1000, '\n');
+	
 	//remove comments
 	rm_comments(input);
-
+	    
 	//use input to construct a Command_Line
-	Command_Line cmd_ln(input);
-
-	for(int i = 0; i < cmd_ln.size(); i++){
-	    if(cmd_ln.at(i)->status == 0)
-		last_cmd_passed= cmd_ln.at(i)->execute();
-
-	    else if(cmd_ln.at(i)->status == 1)
-		if(last_cmd_passed)
-		    last_cmd_passed= cmd_ln.at(i)->execute();
-
-	    else
-		if(!last_cmd_passed)
-		    last_cmd_passed= cmd_ln.at(i)->execute();
-	}
+    	Command_Line cmd_ln(input);
+	
+	//execute commands
+	run = cmd_ln.execute();
     }
  
     return 0;
