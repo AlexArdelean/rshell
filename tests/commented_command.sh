cc2adedaf18335da9af0688b8bbb9061148f0fd2
Alexander Ardelean <aarde001@hammer.cs.ucr.edu>
2016-02-06 15:51:39 -0800

    Updated test cases

---
+++ tests/commented_command.sh
@@ -0,0 +1,9 @@
+echo "ls  #Command with comments"
+ls #Command with comments
+echo
+echo "mkdir test; ls #With more comments"
+mkdir test; ls
+echo
+echo "rmdir test; ls #Remove just created test folder"
+rmdir test; ls #Remove just created test folder
+echo
---
+++ tests/multi_command.sh
@@ -0,0 +1,6 @@
+echo "ls && mkdir test; rmdir test || rmdir test; ls;"
+ls && mkdir test; rm test || rm test; ls;
+echo
+echo "assdd || ls -1"
+ssdd || ls -1
+
